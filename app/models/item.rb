b2f31b37ab8a4c4e7012c455d692a1e0a6a209c6
Komty <komuro566504@gmail.com>
2021-02-28 10:49:51 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do       
+    validates   :name
+    validates   :description
+    validates   :price, numericality: { only_integer: true, length: { in: 300..9,999,999 }
+    validates   :image
+  end
+  
+  with_options presence: true, numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_cost_id
+    validates :prefecture_id
+    validates :delivery_day_id
+  end
+
   belongs_to :user
   has_one_attached :image
 end
