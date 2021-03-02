6bc9517a794284ebb2686d26c08d96aca64b45b2
Komty <komuro566504@gmail.com>
2021-03-02 20:02:59 +0900

    商品削除機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :set_root_path, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_root_path, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   
   
@@ -31,8 +31,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
