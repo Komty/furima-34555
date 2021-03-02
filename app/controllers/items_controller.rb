cc329cb491f0daa51ab3b910895b1bb2542c428b
Komty <komuro566504@gmail.com>
2021-03-02 17:54:23 +0900

    商品情報編集機能　修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_root_path, only: [:edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
   
   
@@ -20,24 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if current_user.id != @item.user.id
-       redirect_to root_path
-    end
-
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render :edit
-    end
   end
 
   private
@@ -45,4 +35,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def set_root_path
+    if current_user.id != @item.user.id
+      redirect_to root_path
+    end
+  end
+  
 end
